449f19e6a9cf9cedf2554bb4a7104156c86f388a
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2017-03-26 18:13:13 +0200

    Add sample invoice used as requirement to test

--- spec/invoices_spec.rb
+++ spec/invoices_spec.rb
@@ -32,6 +32,58 @@ RSpec.describe Invoices do
       expect(invoice.total.before_tax).to eq('7.4'.to_d)
       expect(invoice.total.after_tax).to eq('8.38'.to_d)
     end
+  end
+
+
+
+  context 'creating the sample invoice' do
+    invoice_lines = <<~END
+      PRINGLES CEBOLLA 190 GR         |     2     |     1,7364 € |    10     |
+      BARRITAS HUESITOS PACK 12X20GR  |     1     |     2,3000 € |    10     |
+      COOKIE AVELL CHOC CARREF 200G   |     2     |     0,7364 € |    10     |
+      G.SANDW.YOGUR DIET NAT.GUL220G  |     1     |     1,7273 € |    10     |
+      BARRITA TWIX 6 UNDS X 58 GR     |     2     |     2,5455 € |    10     |
+      CHOC.SNICKERS 4X 50 GRS         |     1     |     1,7909 € |    10     |
+      PASTAS DE TE DELITEA 400 GR     |     1     |     1,7909 € |    10     |
+      TE FRUTAS DEL BOSQUE LIPTON 20  |     1     |     1,7727 € |    10     |
+      PALMERITAS EL PONTON 380 GRS    |     2     |     2,5455 € |    10     |
+      CHEETOS STICKS 96 GRS           |     2     |     1,0727 € |    10     |
+      CHEETOS RIZOS 125GR             |     2     |     1,4909 € |    10     |
+      CHEETOS CRUNCHETOS 130 GRS      |     2     |     1,0182 € |    10     |
+      TUC QUESO 100 GRS               |     2     |     1,0000 € |    10     |
+      TUC CREAM&ONION 100 GRS         |     2     |     1,0000 € |    10     |
+      ENERG.MONSTER ZERO 500ML        |     5     |     0,5091 € |    10     |
+      ENERG.MONSTER ZERO 500ML        |     5     |     1,0364 € |    10     |
+      CROISSANT CACAO CASADO 360G     |     2     |     1,1727 € |    10     |
+      ALMENDRA TOSTAD.COMUN CRF 200G  |     1     |     2,5455 € |    10     |
+      SNATTS PIPAS 4X40 GR            |     1     |     1,9091 € |    10     |
+      TORTITAS MAIZ OLIVAS/CEBOLLINO  |     1     |     1,6545 € |    10     |
+      TORTITA MAIZ QUESO&ALBAHACA124  |     1     |     1,6727 € |    10     |
+      ENERGETICO RED BULL 25 CL P-8   |     3     |     6,6455 € |    10     |
+      20 CUCHA TRANSPARENTES API CA   |     2     |     0,8182 € |    21     |
+      AZUCAR BLANCA AZUCARERA 1,5 KG  |     1     |     0,9727 € |    10     |
+      GOMINOLA HARIBO OSITO ORO 275G  |     1     |     2,0818 € |    10     |
+      PRINGLES XTRA BARBACOA 175G     |     1     |     1,8636 € |    10     |
+      PRINGLES ORIGINAL 190 GR        |     2     |     1,7364 € |    10     |
+      CAFE INTENSO CRF CAPSULAS 30UD  |     1     |     4,9091 € |    10     |
+      CAFE EXTRAFUERTE CRF CAPS 30UD  |     1     |     4,9091 € |    10     |
+      CAFE SUAVE CRF CAPSULAS 30UDS   |     1     |     4,9091 € |    10     |
+      TORTITA ARR C/CHO BICEN 210 G   |     1     |     1,7273 € |    10     |
+      GASTOS DE ENVIO                 |     1     |     4,9587 € |    21     |
+    END
+
+    invoice = Invoices::Invoice.new(invoice_lines.split(/\n/).map do |l|
+      parts = l.split(/\|/)
+      values = [parts[0].strip] + parts[1..3].map { |x| x.tr(',', '.').to_d }
+      Invoices::Line.new(Hash[[:article, :units, :price, :tax_rate].zip(values)])
+    end)
+
+    it 'should have 32 lines' do
+      expect(invoice_lines.split(/\n/).count).to eq(32)
+      expect(invoice.lines.count).to eq(32)
+    end
+
+
 
   end
 
